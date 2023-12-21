2eab7f0a275b8aa8fe1290ee477a794f590fdfb0
Jhon Lennon da Silva santos <jhonuot11@gmail.com>
2023-12-20 21:05:19 -0300

    criei uma funçao para mudar alternar os icones

--- arquivo.js
+++ arquivo.js
@@ -1,35 +1,40 @@
-const button = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-const button3 = document.getElementById('button3');
-const button4 = document.getElementById('button4');
 
-const answer = document.querySelector('.card-answer ');
-const answer2 = document.querySelector('.card-answer2');
-const answer3 = document.querySelector('.card-answer3');
-const answer4 = document.querySelector('.card-answer4');
+//funçao mostrar resposta
 
-const icon = document.getElementById("minus")
+var question =document.querySelectorAll('.card-question');
+var answer = document.querySelectorAll('.card-answer');
 
 
 
-button.addEventListener('click', () => {
-    answer.classList.toggle('active');
+question[0].addEventListener('click', () => {
+    answer[0].classList.toggle('active');
 })
 
-button2.addEventListener('click', () => {
-    answer2.classList.toggle('active') 
+question[1].addEventListener('click', () => {
+    answer[1].classList.toggle('active') 
 })
 
-button3.addEventListener('click', () => {
-    answer3.classList.toggle('active') 
+question[2].addEventListener('click', () => {
+    answer[2].classList.toggle('active') 
 })
 
-button4.addEventListener('click', () => {
-    answer4.classList.toggle('active') 
+question[3].addEventListener('click', () => {
+    answer[3].classList.toggle('active') 
 })
 
 
+// funçao trocar icone
 
+function toggleIcon(element){
+    var button = element.querySelector('button');
+    var icon = button.querySelector('img');
 
+    if(icon.src.includes('icon-plus.svg')){
+        icon.src = 'assets/images/icon-minus.svg'
+    }
 
+    else{
+        icon.src= 'assets/images/icon-plus.svg'
+    }
+}
 
